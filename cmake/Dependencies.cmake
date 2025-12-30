a7085a0615e5200a2318984fa6de0d0d86971a32
Krystian Booker <krystian.booker@gmail.com>
2025-12-30 13:11:26 -0500

    removed vcpkg

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -6,7 +6,13 @@ set(BGFX_BUILD_TOOLS ON CACHE BOOL "" FORCE)
 set(BGFX_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
 set(BGFX_INSTALL OFF CACHE BOOL "" FORCE)
 set(BGFX_AMALGAMATED OFF CACHE BOOL "" FORCE)
-set(BGFX_CONFIG_DEBUG ON CACHE BOOL "" FORCE)
+
+# Enable bgfx debug features only in Debug builds
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    set(BGFX_CONFIG_DEBUG ON CACHE BOOL "" FORCE)
+else()
+    set(BGFX_CONFIG_DEBUG OFF CACHE BOOL "" FORCE)
+endif()
 
 # Add bgfx.cmake
 add_subdirectory(${CMAKE_SOURCE_DIR}/external/bgfx.cmake)
