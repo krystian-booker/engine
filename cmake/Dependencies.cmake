00da02d229cab5c8d0f4893b31936096b5615bb0
Krystian Booker <krystian.booker@gmail.com>
2025-12-31 00:10:02 -0500

    cmake fixes

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -2,23 +2,31 @@
 # External dependencies including bgfx.cmake
 
 # Configure bgfx.cmake options
-set(BGFX_BUILD_TOOLS ON CACHE BOOL "" FORCE)
+set(BGFX_BUILD_TOOLS OFF CACHE BOOL "" FORCE)
 set(BGFX_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
 set(BGFX_INSTALL OFF CACHE BOOL "" FORCE)
 set(BGFX_AMALGAMATED OFF CACHE BOOL "" FORCE)
 
 # Enable bgfx debug features only in Debug builds
-if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-    set(BGFX_CONFIG_DEBUG ON CACHE BOOL "" FORCE)
-else()
-    set(BGFX_CONFIG_DEBUG OFF CACHE BOOL "" FORCE)
+# Note: Only set for single-config generators where CMAKE_BUILD_TYPE is defined
+if(CMAKE_BUILD_TYPE)
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+        set(BGFX_CONFIG_DEBUG ON CACHE BOOL "" FORCE)
+    else()
+        set(BGFX_CONFIG_DEBUG OFF CACHE BOOL "" FORCE)
+    endif()
 endif()
 
 # Add bgfx.cmake
 add_subdirectory(${CMAKE_SOURCE_DIR}/external/bgfx.cmake)
 
-# bgfx.cmake creates these targets:
+# Create namespace aliases
+add_library(bgfx::bgfx ALIAS bgfx)
+add_library(bgfx::bx ALIAS bx)
+add_library(bgfx::bimg ALIAS bimg)
+
+# bgfx.cmake creates raw targets (bgfx, bx, bimg).
 # - bgfx::bgfx      (main library)
 # - bgfx::bx        (base library)
 # - bgfx::bimg      (image library)
-# - shaderc, geometryc, texturec (tools)
+# Tools: shaderc, geometryc, texturec
