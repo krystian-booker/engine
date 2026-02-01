65a708287fcd2546cb150d6c134755b16df9efd5
Krystian Booker <krystian.booker@gmail.com>
2026-02-01 00:22:13 -0500

    cmake clean up

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 @echo off
 call "C:\Program Files\Microsoft Visual Studio\18\Community\Common7\Tools\VsDevCmd.bat" -arch=amd64
+set PATH=%PATH%;C:\Program Files\Microsoft Visual Studio\18\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja
+REM QTDIR is auto-detected by CMakeLists.txt - no need to set manually
 echo Configuring...
 cmake --preset Qt-Debug -S C:\Users\krystian\source\repos\engine
 echo Building...
