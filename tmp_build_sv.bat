e90ea2c75b184380189129346e91a66edbd3648a
krystian-booker <krystian.booker@gmail.com>
2026-03-15 13:12:42 -0400

    cleanup

--- tmp_build_sv.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath`) do set "VS_PATH=%%i"
-call "%VS_PATH%\Common7\Tools\VsDevCmd.bat" -arch=amd64 >/dev/null 2>&1
-cd /d "C:\Users\Krystian\source\repos\engine"
-cmake --build out/build/Qt-Debug --target shadow_validation 2>&1
